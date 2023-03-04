e4cec9843cabdf649bfec0992fa9c721727374db
Holly Leal <94670410+hollyhleal@users.noreply.github.com>
2023-03-03 16:53:31 -0800

    add README template in generateFile function

--- index.js
+++ index.js
@@ -12,22 +12,47 @@ inquirer
     },
   ])
   .then((responses) => {
-    const htmlContent = generateHTML(responses);
-    fs.writeFile("README.md", htmlContent, (error) =>
+    const pageContent = generateFile(responses);
+    fs.writeFile("README.md", pageContent, (error) =>
       error ? console.log(error) : console.log("Your README has been created!")
     );
   });
 
-function generateHTML(responses) {
-  const HTML = ``;
-  return HTML;
+function generateFile(responses) {
+  const readMe = `# Title
+
+  ## Description
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  
+  ## Installation
+  
+  ## Usage
+  
+  ## License
+  
+  ## Contributing
+  
+  ## Tests
+  
+  ## Questions
+  
+  For questions, comments or concerns you can reach me at or .`;
+  return readMe;
 }
 
+console.log(responses);
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
