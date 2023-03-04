084d0e68ffad3b50803ab3817e131eff2487dfd7
Holly Leal <94670410+hollyhleal@users.noreply.github.com>
2023-03-03 16:33:35 -0800

    add dependencies and inquirer .prompt, shell for generateHTML function

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-// TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
+const inquirer = require("inquirer"); //object of methods
+const fs = require("fs");
+//Create an array of questions for user input
 const questions = [];
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your project title?",
+      name: "title",
+    },
+  ])
+  .then((responses) => {
+    const htmlContent = generateHTML(responses);
+    fs.writeFile("README.md", htmlContent, (error) =>
+      error ? console.log(error) : console.log("Your README has been created!")
+    );
+  });
+
+function generateHTML(responses) {
+  const HTML = ``;
+  return HTML;
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
