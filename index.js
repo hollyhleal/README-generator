fe3c57e3adfa0c7340dc050ae24e5e1e98635f88
Holly Leal <94670410+hollyhleal@users.noreply.github.com>
2023-03-03 20:05:39 -0800

    add rest of inputs and update readme structure

--- index.js
+++ index.js
@@ -15,6 +15,37 @@ inquirer
       message: "Please give a description of your project.",
       name: "description",
     },
+    {
+      type: "input",
+      message: "What are the steps required to install your project?",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "Provide instructions and examples for use.",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "Please list any collaborators or third-party assets used.",
+      name: "credits",
+    },
+    {
+      type: "input",
+      message:
+        "Write tests for your application. Then provide examples on how to run them here.",
+      name: "tests",
+    },
+    {
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "github",
+    },
+    {
+      type: "input",
+      message: "What is your email address?",
+      name: "email",
+    },
   ])
   .then((responses) => {
     const pageContent = generateFile(responses);
@@ -27,27 +58,34 @@ function generateFile(responses) {
   const readMe = `# ${responses.title}
 
   ## Description
+  ${responses.description}
   
   ## Table of Contents
   
   - [Installation](#installation)
   - [Usage](#usage)
-  - [Credits](#credits)
   - [License](#license)
+  - [Credits](#credits)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
   
   ## Installation
+  ${responses.installation}
   
   ## Usage
+  ${responses.usage}
   
   ## License
   
-  ## Contributing
+  ## Credits
+  ${responses.credits}
   
   ## Tests
+  ${responses.tests}
   
   ## Questions
-  
-  For questions, comments or concerns you can reach me at or .`;
+  For questions, comments or concerns you can reach me at [https://github.com/${responses.github}](https://github.com/${responses.github}) or [${responses.email}](mailto:${responses.email}).`;
   return readMe;
 }
 
